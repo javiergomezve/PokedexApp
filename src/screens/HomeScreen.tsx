de3dcbc480f7fc7aaf88ec216e68a3cc550fba26
Javier Gómez <javiergomezve@gmail.com>
2022-01-29 23:30:56 -0500

    [ADD] HomeScreen: pokemon list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, FlatList, ActivityIndicator} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import PokemonCard from '../components/PokemonCard';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 
 import {styles} from '../theme/appTheme';
 
 const HomeScreen = () => {
     const {top} = useSafeAreaInsets();
+
+    const {isLoading, simplePokemonList, loadPokemons} = usePokemonPaginated();
 
     return (
         <>
@@ -14,14 +18,39 @@ const HomeScreen = () => {
                 style={styles.pokebolaBg}
             />
 
-            <Text
+            <View
                 style={{
-                    ...styles.title,
-                    ...styles.globalMargin,
-                    top: top + 20,
+                    alignItems: 'center',
                 }}>
-                Pokedex
-            </Text>
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    ListHeaderComponent={
+                        <Text
+                            style={{
+                                ...styles.title,
+                                ...styles.globalMargin,
+                                top: top + 20,
+                                marginBottom: top + 20,
+                                paddingBottom: 10,
+                            }}>
+                            Pokedex
+                        </Text>
+                    }
+                    data={simplePokemonList}
+                    keyExtractor={pokemon => pokemon.id}
+                    renderItem={({item}) => <PokemonCard pokemon={item} />}
+                    onEndReached={loadPokemons}
+                    onEndReachedThreshold={0.4}
+                    ListFooterComponent={
+                        <ActivityIndicator
+                            style={{height: 100}}
+                            size={30}
+                            color="gray"
+                        />
+                    }
+                />
+            </View>
         </>
     );
 };
