db8acd03a20a95d366da2dab9384dced0cee26d4
Javier Gómez <javiergomezve@gmail.com>
2022-01-29 19:37:08 -0500

    [ADD] Stack navigation

---
+++ src/components/Navigator/Navigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from '../../screens/HomeScreen';
+import PokemonScreen from '../../screens/PokemonScreen';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Pokemon" component={PokemonScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default Navigator;
