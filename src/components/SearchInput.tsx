6ebeab7c5cd7ddf2e0adf45b12c56c103f1b044e
Javier Gómez <javiergomezve@gmail.com>
2022-01-30 19:52:40 -0500

    [ADD} Search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet, TextInput, View, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const SearchInput = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.textBackground}>
+                <TextInput
+                    placeholder="Search pokemon"
+                    style={{
+                        ...styles.textInput,
+                        top: Platform.OS === 'ios' ? 0 : 2,
+                    }}
+                    autoCapitalize={false}
+                    autoCorrect={false}
+                />
+
+                <Icon name="search-outline" color="grey" size={30} />
+            </View>
+        </View>
+    );
+};
+
+export default SearchInput;
+
+const styles = StyleSheet.create({
+    container: {},
+    textBackground: {
+        backgroundColor: '#F3F1F3',
+        borderRadius: 50,
+        height: 40,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadwRiu: 3.84,
+        elevation: 5,
+    },
+    textInput: {
+        flex: 1,
+        fontSize: 18,
+    },
+});
